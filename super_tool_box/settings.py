29e95854a94c9ba137ed36c3afe164b91bc464c2
jeffer.gu <jeffery.gu@cootek.cn>
2015-09-15 20:23:07 +0800

    page template

--- super_tool_box/settings.py
+++ super_tool_box/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
 
     'qunar_flight_query',
+    'chart_tools',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,7 +58,8 @@ ROOT_URLCONF = 'super_tool_box.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                    'django.template.loaders.app_directories.Loader'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
